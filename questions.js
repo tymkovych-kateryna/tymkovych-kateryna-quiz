64e777381438569bd39bc6942f6c6ae009e5a187
tymkovych-kateryna <116115176+tymkovych-kateryna@users.noreply.github.com>
2023-09-02 23:02:32 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does the HTML acronym stand for?",
+        answer: "a) HyperText Markup Language",
+        options: [
+            "a) HyperText Markup Language",
+            "b) HighText Machine Language",
+            "c) Hyperlinks and Text Markup Language",
+            "d) Home Tool Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which CSS property is used to change the background color of an element?",
+        answer: "c) background-color",
+        options: [
+            "a) color",
+            "b) text-color",
+            "c) background-color",
+            "d) bgcolor"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the correct syntax for declaring a JavaScript variable?",
+        answer: "d) var myVariable;",
+        options: [
+            "a) var = myVariable;",
+            "b) myVariable = var;",
+            "c) variable myVariable;",
+            "d) var myVariable;"
+        ]
+    },
+    {
+        numb: 4,
+        question: "In HTML, what element is used to display a clickable link?",
+        answer: "d) Anchor",
+        options: [
+            "a) Image",
+            "b) Button",
+            "c) Link",
+            "d) Anchor"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following is a JavaScript framework/library?",
+        answer: "b) jQuery",
+        options: [
+            "a) Bootstrap",
+            "b) jQuery",
+            "c) Django",
+            "d) Laravel"
+        ]
+    }
+];
